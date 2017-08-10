65140f040ebccf1bba2728ae40238b27ef481907
mmarovich <mmarovich@gmail.com>
2017-08-10 18:48:30 -0500

    migrated server.js to index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,125 @@
 const path = require('path');
 const express = require('express');
 
+const passport = require('passport');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const morgan = require('morgan');
+const MongoStore = require('connect-mongo')(session);
+mongoose.Promise = global.Promise;
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const { PORT, DATABASE_URL, TEST_DATABASE_URL } = require('./config/database.js');
+const nev = require('email-verification')(mongoose);
+const User = require('./routes/models/user');
+const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt-nodejs');
+
 const app = express();
 
-// API endpoints go here!
+require('./config/passport')(passport)
 
-app.get('/api/test', (req, res) => {
-    return res.json({response: 'api/test reached'})
-})
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(session({
+  name: 'Session',
+  resave: true,
+  saveUninitialized: true,
+  secret: 'forty-two',
+  store: new MongoStore({
+    url: DATABASE_URL,
+    autoReconnect: true
+  })
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+
+//////////////// nev
+var myHasher = function (password, tempUserData, insertTempUser, callback) {
+  console.log(arguments);
+  var hash = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  return insertTempUser(hash, tempUserData, callback);
+};
+
+function setUpNev() {
+
+  nev.configure({
+    verificationURL: 'https://rocky-escarpment-61736.herokuapp.com/api/email-verification/${URL}',
+    URLLength: 48,
+
+    // mongo-stuff
+    persistentUserModel: User,
+    // tempUserModel: TempUser,
+    // tempUserCollection: 'temporary_users',
+    emailFieldName: 'email',
+    passwordFieldName: 'password',
+    URLFieldName: 'GENERATED_VERIFYING_URL',
+    expirationTime: 86400,
+    hashingFunction: myHasher,
+    // emailing options
+    transportOptions: {
+      service: 'Gmail',
+      auth: {
+        user: 'phoebusapollotest@gmail.com',
+        pass: 'Sup3rS3cr3t'
+      }
+    },
+    verifyMailOptions: {
+      from: 'Do Not Reply <phoebusapollotest@gmail.com>',
+      subject: 'Confirm your account',
+      html: '<p>Please verify your account within 24 hours by clicking <a href="${URL}">this link</a>. If you are unable to do so, copy and ' +
+                'paste the following link into your browser:</p><p>${URL}</p>',
+      text: 'Please verify your account by clicking the following link, or by copying and pasting it into your browser: ${URL}'
+    },
+    shouldSendConfirmation: true,
+    confirmMailOptions: {
+      from: 'Do Not Reply <phoebusapollotest@gmail.com>',
+      subject: 'Successfully verified!',
+      html: '<p>Your account has been successfully verified. If you haven\'t already, you may <a href="https://rocky-escarpment-61736.herokuapp.com/api/login">log in here.</a></p>',
+      text: 'Your account has been successfully verified.'
+    },
+
+    // hashingFunction: null,
+    smtpTransport: nodemailer.createTransport({
+      from: 'phoebusapollotest@gmail.com',
+      options: {
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+          user: 'phoebusapollotest@gmail.com',
+          pass: 'Sup3rS3cr3t'
+        }
+      }
+    })
+  }, function (error, options) {
+  });
+
+  nev.generateTempUserModel(User, function (err, tempUserModel) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    
+    console.log('generated temp user model: ' + (typeof tempUserModel === 'function'));
+  });
+
+}
+////////////////// end nev
+
+require('./routes/userRouter.js')(app, passport, nev);
+
+// API endpoints go here!
 
 // Serve the built client
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -20,29 +132,42 @@ app.get(/^(?!\/api(\/|$))/, (req, res) => {
 });
 
 let server;
-function runServer(port=3001) {
-    return new Promise((resolve, reject) => {
-        server = app.listen(port, () => {
-            resolve();
-        }).on('error', reject);
-    });
-}
-
-function closeServer() {
-    return new Promise((resolve, reject) => {
-        server.close(err => {
-            if (err) {
-                return reject(err);
-            }
-            resolve();
+function runServer(database = DATABASE_URL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(database, err => {
+      if (err) {
+        return reject(err);
+      }
+      setUpNev();
+      server = app.listen(PORT, () => {
+        console.log(`Your app is listening on port ${PORT}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
         });
     });
-}
+  });
+};
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          console.log(`closeServer error, ${err}`);
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+};
 
 if (require.main === module) {
-    runServer();
-}
-
-module.exports = {
-    app, runServer, closeServer
+  runServer().catch(err => console.error(err));
 };
+
+module.exports = { app, runServer, closeServer };
